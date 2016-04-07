1835637b4327df50c1e3ba6089722947f5daf8cf
nathanieljevans_work <Nathaniel.Evansj@gmail.com>
2016-04-07 08:57:48 -0700

    Fixed Class Syntax
    The problem with the class syntax was 1) a damn parentheses and 2) the
    class definition needs to be sequentially before the method that it is
    instantiated in. Probably be worth making a library for all the classes
    and import it in, just to avoid this kind of problem.
    
    Also added user input to chose path name based on location.

--- predictive_sub_identification.py
+++ predictive_sub_identification.py
@@ -9,19 +9,52 @@ project description and necessary .csv files available at: https://www.innocenti
 import csv
 
 # CSV files stored under the Downloads folder
-data_pathname = "C:\\Users\\Nate\\Downloads\\InnoCentive_9933623_Data.csv" 
-example_submit_pathname = "C:\\Users\\Nate\\Downloads\\InnoCentive_9933623_example_submit.csv" 
+data_pathname_WORK = "C:\\Users\\Administrator\\Downloads\\Innocentive_9933623_Data.csv" 
+data_pathname_HOME = "C:\\Users\\Nate\\Downloads\\InnoCentive_9933623_Data.csv" 
+example_submit_pathname_HOME = "C:\\Users\\Nate\\Downloads\\InnoCentive_9933623_example_submit.csv" 
 training_data_pathname = "C:\\Users\\Nate\\Downloads\\InnoCentive_9933623_Training_Data.csv" 
 truth_subjects_pathname = "C:\\Users\\Nate\\Downloads\\InnoCentive_9933623_Training_Data_truth_subjects.csv" 
 
+
+patient_list = []
+
+class Patient: 
+    '''
+    unique_id = 0 # this might be unnecessary since the datasets are unrelated... I think? 
+    dataset = "" 
+    data_id = "" # this is a the count within a dataset, not unique
+    y_response = 0.0 # more negative values showing a better treatment effect :: "meaningfully better" is anything less then -0.6
+    gen_char = [] # vals can be 0,1,2, called x1 through x20 in the description
+    patient_char = [] # these values are continuous
+    '''
+    
+    def __init__ (self, data, d_id, treatment, y, gen, pat): 
+        self.dataset = data 
+        self .id = d_id 
+        self.trt = treatment
+        self.y_response = y 
+        self.gen_char = gen 
+        self.patient_char = pat     
+    
 def main() : 
-    data_file = list(csv.reader(open(data_pathname)))
-    number_patients = len(data_file) - 1  # Number of rows in file except header 
-    print ("number patients by row: " + number_patients)
-    patient_list = []
+    path_in = input("Which pathname to use (work/home)")
+    path = ""
+    if (path_in == "work"): 
+        path = data_pathname_WORK 
+    elif (path_in == "home"): 
+        path = data_pathname_HOME 
+    else : 
+        print("Not a valid path option")
+    
+    data_file = list(csv.reader(open(path)))
+    
     for i,patient in enumerate(data_file): 
         gen_list = [] 
         pat_list = []
+        dataset = ""
+        data_id = "" 
+        treatment = -1 
+        y_response = 0.0
         if(i != 0):  
             dataset = patient[0] 
             data_id = patient[1]
@@ -33,29 +66,10 @@ def main() :
                         gen_list.append(x)
                     else: 
                         pat_list.append(x)
-                        
-        patient_list.append(patient(dataset,data_id,treatment,y_response,gen_list,pat_list))
+          
+        p =  Patient(dataset,data_id,treatment,y_response,gen_list,pat_list)               
+        patient_list.append(p)
       
-    print ("Patient length check " + len(patient_list)
-
-
-class patient: 
-    """
-    unique_id = 0 # this might be unnecessary since the datasets are unrelated... I think? 
-    dataset = "" 
-    data_id = "" # this is a the count within a dataset, not unique
-    y_response = 0.0 # more negative values showing a better treatment effect :: "meaningfully better" is anything less then -0.6
-    gen_char = [-1]*20 # vals can be 0,1,2, called x1 through x20 in the description
-    patient_char = [-1.0]*20 # these values are continuous
-    """
-    
-    def __init__ (self, data, d_id, treatment, y, gen, pat): 
-        self.dataset = data 
-        self .id = d_id 
-        self.trt = treatment
-        self.y_response = y 
-        self.gen_char = gen 
-        self.patient_char = pat 
-    
+    print ("Patient length check " + str(len(patient_list)))
 
 main() 
