9b8cc1723850768bdd756e0b46eb40db443fa3d9
nathanieljevans <Nathaniel.Evansj@gmail.com>
2016-04-06 21:03:53 -0700

    main python script
    This is the initial python script

---
+++ predictive_sub_identification.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Innocentive Challenge: Predictive Subgroup Identification 
+@author: Nate Evans 
+Creation Date: 4/6/16
+project description and necessary .csv files available at: https://www.innocentive.com/ar/workspace/challengeDetail?challenge=9933623
+"""
+
+import csv
+
+# CSV files stored under the Downloads folder
+data_pathname = "C:\\Users\\Nate\\Downloads\\InnoCentive_9933623_Data.csv" 
+example_submit_pathname = "C:\\Users\\Nate\\Downloads\\InnoCentive_9933623_example_submit.csv" 
+training_data_pathname = "C:\\Users\\Nate\\Downloads\\InnoCentive_9933623_Training_Data.csv" 
+truth_subjects_pathname = "C:\\Users\\Nate\\Downloads\\InnoCentive_9933623_Training_Data_truth_subjects.csv" 
+
+def main() : 
+    # whatever 
+
+
+main() 
